099bcbab2971f91caf87a83ac2855f383d615fce
choudayer34-afk <choudayer34@gmail.com>
2026-08-01 23:13:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -270,8 +270,8 @@ const inboxTitle =
 
 if (inboxTitle) {
 
-    inboxTitle.textContent =
-        📥 À trier (${envies.length});
+    inboxTitle.textContent =`
+        📥 À trier (${envies.length})`;
 
 }
 
