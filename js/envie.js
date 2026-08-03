fdcf1411d0477ee5cb3c868e58d64538b3d18525
choudayer34-afk <choudayer34@gmail.com>
2026-08-03 04:13:24 +0200

    Update envie.js

--- js/envie.js
+++ js/envie.js
@@ -60,95 +60,10 @@ export function openEnvie(id) {
     document.getElementById("ficheDescription").value =
         envie.description || "";
         
-        const checklist =
-    document.getElementById("checklistContainer");
-
-checklist.innerHTML = "";
-
-(envie.checklist || []).forEach(item=>{
-
-    const row =
-        document.createElement("div");
-
-    row.className="checklistRow";
-
-    row.innerHTML = `
-
-<label class="checkLabel">
-
-    <input
-        type="checkbox"
-        ${item.checked ? "checked" : ""}>
-
-    <span>${item.texte}</span>
-
-</label>
-
-<button
-    class="deleteChecklistButton">
-
-    🗑️
-
-</button>
-
-`;
-
-    row.querySelector("input")
-        .addEventListener("change",()=>{
-
-            toggleChecklistItem(
-                currentEnvieId,
-                item.id
-            );
-
-            openEnvie(currentEnvieId);
-
-        });
-row.querySelector(".deleteChecklistButton")
-    .addEventListener("click", (event) => {
-
-        event.stopPropagation();
-
-        deleteChecklistItem(
-            currentEnvieId,
-            item.id
-        );
-
-        openEnvie(currentEnvieId);
-
-        showToast("✓ Élément supprimé");
-
-    });
-    checklist.appendChild(row);
-
-});
+       renderChecklist(envie);
 
 
-
-        const urlList =
-    document.getElementById("urlList");
-
-urlList.innerHTML = "";
-
-(envie.urls || []).forEach(link => {
-
-    const div =
-        document.createElement("div");
-
-    div.className = "urlItem";
-
-    div.innerHTML = `
-        <a href="${link.url}"
-           target="_blank">
-
-            ${link.url}
-
-        </a>
-    `;
-
-    urlList.appendChild(div);
-
-});
+        renderUrls(envie);
 
         document.getElementById("ficheLieu").value =
     envie.lieu?.nom || "";
