f9c1f2b358353b894b8199e5d207f4703b016799
choudayer34-afk <choudayer34@gmail.com>
2026-08-03 03:58:05 +0200

    Create envie.js

---
+++ js/envie.js
@@ -0,0 +1,35 @@
+export function openEnvie(id){
+
+}
+
+export function closeFiche(){
+
+}
+
+export function renderHeader(envie){
+
+}
+
+export function renderCategorie(envie){
+
+}
+
+export function renderPeriode(envie){
+
+}
+
+export function renderLieu(envie){
+
+}
+
+export function renderChecklist(envie){
+
+}
+
+export function renderUrls(envie){
+
+}
+
+export function renderDescription(envie){
+
+}
