b17fba366ab5bc73ee6607470cc7c45236056853
choudayer34-afk <choudayer34@gmail.com>
2026-08-03 05:07:24 +0200

    Update urls.js

--- js/urls.js
+++ js/urls.js
@@ -1,11 +1,27 @@
+export function renderUrls(envie) {
+
+    const urlList = document.getElementById("urlList");
+    urlList.innerHTML = "";
+
+    (envie.urls || []).forEach(link => {
+
+        const div = document.createElement("div");
+        div.className = "urlItem";
+
+        div.innerHTML = `<a href="${link.url}" target="_blank">${link.url}</a>`;
+
+        urlList.appendChild(div);
+
+    });
+
+}
+
+
 export function addUrl(){
 
 }
 
 export function deleteUrl(){
 
-}
-
-export function renderUrls(){
 
 }
