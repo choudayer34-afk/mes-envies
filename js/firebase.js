960268e10b747cd8cf13adeda02a743d08512d2c
choudayer34-afk <choudayer34@gmail.com>
2026-08-03 15:09:29 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,16 @@
- 
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB42HSFoWRH_KfMrcWy3U7kdTjFPJbarJU",
+  authDomain: "mes-envies-21527.firebaseapp.com",
+  projectId: "mes-envies-21527",
+  storageBucket: "mes-envies-21527.firebasestorage.app",
+  messagingSenderId: "225833325776",
+  appId: "1:225833325776:web:5f80a962f6579618c63ad5"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
