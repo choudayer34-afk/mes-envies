d683180ee859bf1e41c0fff5ce98e6d1aa2ba2aa
choudayer34-afk <choudayer34@gmail.com>
2026-08-05 21:28:41 +0200

    Update plus.js

--- js/plus.js
+++ js/plus.js
@@ -1,29 +1,26 @@
+import { openJeux } from "./jeux.js";
+import { openSurvie } from "./survie.js";
 import { initHomeMeteo } from "./ui.js";
-
 
 export function initPlus() {
 
     document.getElementById("btnPlus").addEventListener("click", () => {
         document.getElementById("plusModal").classList.remove("hidden");
+        initHomeMeteo();
     });
 
     document.getElementById("closePlus").addEventListener("click", () => {
         document.getElementById("plusModal").classList.add("hidden");
     });
-    document.getElementById("btnPlus").addEventListener("click", () => {
-        document.getElementById("plusModal").classList.remove("hidden");
-        initHomeMeteo();
-    });
 
-        document.getElementById("plusBtnJeux").addEventListener("click", () => {
+    document.getElementById("plusBtnJeux").addEventListener("click", () => {
         document.getElementById("plusModal").classList.add("hidden");
-        document.getElementById("btnJeux")?.click();
+        openJeux();
     });
 
     document.getElementById("plusBtnSurvie").addEventListener("click", () => {
         document.getElementById("plusModal").classList.add("hidden");
-        document.getElementById("btnSurvie")?.click();
+        openSurvie();
     });
-
 
 }
