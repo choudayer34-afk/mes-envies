f52a6fcdc0c8983798598cec6cb80fe7422a1652
choudayer34-afk <choudayer34@gmail.com>
2026-08-05 09:34:57 +0200

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -65,12 +65,15 @@ export function initPhotos() {
 
 }
 
-export async function uploadToCloudinary(file) {
-
+export async function uploadToCloudinary(file, customName = null) {
 
     const formData = new FormData();
     formData.append("file", file);
     formData.append("upload_preset", UPLOAD_PRESET);
+
+    if (customName) {
+        formData.append("public_id", customName.replace(/\.[^/.]+$/, ""));
+    }
 
     const response = await fetch(
         `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
@@ -84,6 +87,8 @@ export async function uploadToCloudinary(file) {
     return response.json();
 
 }
+
+
 
 export function renderPhotosGrid(envie) {
 
