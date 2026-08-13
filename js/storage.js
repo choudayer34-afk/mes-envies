e079c3d6df8c695d01735f33ca499c484fb5eb47
choudayer34-afk <choudayer34@gmail.com>
2026-08-13 16:01:30 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -14,6 +14,10 @@ export function getChangelog() {
     return [...changelogCache].sort((a, b) => (b.date || 0) - (a.date || 0));
 }
 
+export function updateEnvieStatutManuel(id, statutManuel) {
+    patchEnvie(id, { statutManuel });
+}
+
 export function updateEnvieRubriquesEtat(id, rubriquesEtatManuel) {
     patchEnvie(id, { rubriquesEtatManuel });
 }
